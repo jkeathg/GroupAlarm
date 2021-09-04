70b9f7bb1fb456846e9db45f2b58c6993df2f0c5
Jeremy <jkeathg@hotmail.com>
2021-09-04 17:41:57 -0400

    main page look created, js for drop down created

--- app.js
+++ app.js
@@ -7,3 +7,20 @@
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+
+
+// nav dropdown
+let dropdown = document.getElementById('dropdown');
+let dropdownContent = document.getElementById('dropdown-content');
+
+
+// on click show drop down
+dropdown.addEventListener('click', () => {
+    if (dropdownContent.style.display === 'none'){
+        dropdownContent.style.display = 'block';
+    } else {
+        dropdownContent.style.display = 'none';
+    }
+
+});
