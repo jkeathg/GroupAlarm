a3211e848565054bf52d83105ba5508ca64e977f
Jeremy <jkeathg@hotmail.com>
2021-09-13 19:30:34 -0400

    Added pages for when logged in. Needs the close button to close login windows and logic to prevent multiple forms from being open. Add Alarm Form needs created, manage friends and preferences

---
+++ LoggedInHome.js
@@ -0,0 +1,71 @@
+// import functions
+
+// reference needed DOM elements
+
+
+// set event listeners 
+    // get user input(s)
+    // do any needed work with the value(s)
+    // update DOM to reflect new value(s)
+
+
+
+// nav dropdown
+let dropdown = document.getElementById('dropdown');
+let dropdownContent = document.getElementById('dropdown-content');
+
+
+// on click show drop down
+dropdown.addEventListener('click', () => {
+    if (dropdownContent.style.display === 'none'){
+        dropdownContent.style.display = 'block';
+    } else {
+        dropdownContent.style.display = 'none';
+    }
+
+});
+
+
+// // get signup form
+// let body = document.getElementsByTagName('body');
+// let signupLink = document.getElementById('sign-up');
+// let signupForm = document.getElementById('signup-form');
+
+
+
+// signupLink.addEventListener('click', () => {
+//     if (signupForm.style.display === 'none' || loginForm.style.display === 'none' || addFriendForm.style.display === 'none') {
+//         signupForm.style.display = 'block';
+        
+
+//     } else {
+//         signupForm.style.display = 'none';
+//     }
+// });
+
+
+// // get login form
+// let loginLink = document.getElementById('log-in');
+// let loginForm = document.getElementById('login-form');
+
+// loginLink.addEventListener('click', () => {
+//     if (loginForm.style.display === 'none' || signupForm.style.display === 'none' || addFriendForm.style.display === 'none') {
+//         loginForm.style.display = 'block';
+//     } else {
+//         loginForm.style.display = 'none';
+//     }
+// });
+
+
+// get add form
+
+let addFriendLink = document.getElementById('friend-add');
+let addFriendForm = document.getElementById('adding-friend');
+
+addFriendLink.addEventListener('click', () => {
+    if (addFriendForm.style.display === 'none') {
+        addFriendForm.style.display = 'block';
+    } else {
+        addFriendForm.style.display = 'none';
+    }
+});
--- app.js
+++ app.js
@@ -34,7 +34,7 @@ let signupForm = document.getElementById('signup-form');
 
 
 signupLink.addEventListener('click', () => {
-    if (signupForm.style.display === 'none' && loginForm.style.display === 'none') {
+    if (signupForm.style.display === 'none' || loginForm.style.display === 'none' || addFriendForm.style.display === 'none') {
         signupForm.style.display = 'block';
         
 
@@ -49,9 +49,23 @@ let loginLink = document.getElementById('log-in');
 let loginForm = document.getElementById('login-form');
 
 loginLink.addEventListener('click', () => {
-    if (loginForm.style.display === 'none' && signupForm.style.display === 'none') {
+    if (loginForm.style.display === 'none' || signupForm.style.display === 'none' || addFriendForm.style.display === 'none') {
         loginForm.style.display = 'block';
     } else {
         loginForm.style.display = 'none';
+    }
+});
+
+
+// get add form
+
+let addFriendLink = document.getElementById('adding-friend');
+let addFriendForm = document.getElementById('friend-form');
+
+addFriendLink.addEventListener('click', () => {
+    if (addFriendForm.style.display === 'none' || signupForm.style.display === 'none' || loginForm.style.display === 'none') {
+        addFriendForm.style.display = 'block';
+    } else {
+        addFriendForm.style.display = 'none';
     }
 });
